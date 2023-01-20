0e32f392c55bb47e39e1e10a6ea73b072be59c69
Manoj <manuvini29@gmail.com>
2023-01-20 09:38:00 +0530

    20th jan

--- src/whiteboard/PresenterVideo.js
+++ src/whiteboard/PresenterVideo.js
@@ -1,59 +1,63 @@
-import Canvaboard from "../whiteboard/canvaboard";
-import React, { Component } from "react";
 import "../whiteboard/presentervideo.css";
-import { Button } from "antd";
+import React, { useRef, useState } from "react";
 
-class Presentervideo extends React.Component {
-  constructor(props) {
-    super(props);
+function Presentervideo() {
+  const pos = getXYPosition(36.5, 45);
+  const [position, setPosition] = useState({ x: pos.x, y: pos.y });
+  const videoRef = useRef(null);
 
-    this.state = {
-      localStream: null,
+  const handleMouseDown = (event) => {
+    const initialX = event.clientX - position.x;
+    const initialY = event.clientY - position.y;
+
+    const handleMouseMove = (event) => {
+      console.log(event.clientX, event.clientY);
+      setPosition({
+        x: event.clientX - initialX,
+        y: event.clientY - initialY,
+      });
     };
-  }
 
-  componentDidMount() {
-    this.getLocalStream();
-  }
+    document.addEventListener("mousemove", handleMouseMove);
 
-  getLocalStream = () => {
-    // const success = (stream) => {
-    //   window.localStream = stream
-    //   this.setState({
-    //     localStream: stream
-    //   })
-    // var video = document.querySelector("video2");
-    // // video.srcObject = this.state.localStream
-    // this.props.childToParent(video);
-    // }
-    // const failure = (e) => {
-    //   console.log('getUserMedia Error: ', e)
-    // }
-    // const constraints = {
-    //   audio: true,
-    //   video: true,
-    //   options: {
-    //     mirror: true,
-    //   }
-    // }
-    // navigator.mediaDevices.getUserMedia(constraints)
-    //   .then(success)
-    //   .catch(failure)
+    const handleMouseUp = () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+      document.removeEventListener("mouseup", handleMouseUp);
+    };
+
+    document.addEventListener("mouseup", handleMouseUp);
   };
 
-  render() {
-    return (
-      <div class="presenter-box">
-        {/* <video autoPlay={true} id='video2'  class="video2"/> */}
-        <video
-          class="presentervideo"
-          autoPlay={true}
-          playsInline
-          id="presentervideo"
-        ></video>
-      </div>
-    );
-  }
+  return (
+    <div
+      className="presenter-box"
+      ref={videoRef}
+      onMouseDown={handleMouseDown}
+      style={{
+        position: "absolute",
+        left: position.x,
+        top: position.y,
+        zIndex: 999,
+      }}
+    >
+      <video
+        className="presentervideo"
+        autoPlay={true}
+        playsInline
+        id="presentervideo"
+      ></video>
+    </div>
+  );
+}
+
+function getXYPosition(percentageFromRight, fixedPixelValue) {
+  // Get the width of the parent element
+  const parentWidth = document.body.offsetWidth;
+  // Calculate the x position by subtracting the percentage from 100
+  const x = ((100 - percentageFromRight) / 100) * parentWidth;
+  // Set the y position as the fixed pixel value
+  const y = fixedPixelValue;
+  return { x: x, y: y };
 }
 
 export default Presentervideo;
